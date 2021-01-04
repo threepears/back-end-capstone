cad610c16252f5a7c5ea7a420dc94da78db5feb7
Michael Conrad <michael@threepears.com>
2021-01-03 21:26:01 -0500

    Add another log ...

--- routes/postgres.js
+++ routes/postgres.js
@@ -97,7 +97,7 @@ router.patch("/updatestocks", async (_req, res) => {
         request('https://cloud.iexapis.com/stable/stock/' + each + '/quote?token=' + API_KEY, async (_error, _response, body) => {
           try {
             let price
-            console.log("GETTING STOCKS BODY CHECK", body)
+            console.log("GETTING STOCKS BODY CHECK", body, typeof(body))
             if (body !== "Unknown symbol" || body !== "Not found") {
               let allStockRows = []
               console.log("GETTING READY TO PARSE", body)
